32424f86f1492a4788834270286de3610c91756f
Lothar Zok <lothar.zok@web.de>
2023-05-25 07:30:45 +0200

    Added storage.js to every html

---
+++ js/storage.js
@@ -0,0 +1,14 @@
+const STORAGE_TOKEN = 'IOGUK15K4QR1H4E6Z8358ZBEFF667PNX3VN95C2Z';
+const STORAGE_URL   = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, valeu) {
+    const payload = {key, value, token: STORAGE_TOKEN};
+    return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)});
+}
+
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
