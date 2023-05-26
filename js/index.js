850b804e44f94ac70c742c210482ea12c84d2474
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-05-26 14:10:24 +0200

    added input tag

--- js/index.js
+++ js/index.js
@@ -49,27 +49,20 @@ function renderSignUpWindow() {
 */
 function returnSignUpHtml() {
 return /*html*/ `
-<img src="img/backArrow.svg" class="backArrow" onclick="renderLogInWindow()" alt="">
-<h1>Sign Up</h1>
-<div class="blueLine"></div>
-<div class="inputContainer">
-	<input type="text" id="name" placeholder="Name" class="inputField"><img src="img/person.svg" alt="">
-</div>
-<div class="inputContainer">
-	<input type="email" id="email" placeholder="Email" class="inputField"><img src="img/letter.svg" alt="">
-</div>
-<div class="inputContainer">
-	<input type="text" id="password" placeholder="Password" class="inputField"><img src="img/lock.svg" alt="">
-</div>
-<div class="rememberContainer">
-	<div class="checkContainer">
-		<input type="checkbox" name="remember" id="remember" id="remember"> <p>Remember me</p>
-	</div>    
-	<a href="#" class="link">Forgot my password</a>
-</div>
-<div class="twiceBtnContainer">
-	<button class="focusBtn">Sign up</button>
-	<button class="outFocusBtn">Guest Log in</button>
-</div>
+	<img src="img/backArrow.svg" class="backArrow" onclick="renderLogInWindow()" alt="">
+	<h1>Sign Up</h1>
+	<div class="blueLine"></div>
+	<div class="inputContainer">
+		<input type="text" id="name" placeholder="Name" class="inputField"><img src="img/person.svg" alt="">
+	</div>
+	<div class="inputContainer">
+		<input type="email" id="email" placeholder="Email" class="inputField"><img src="img/letter.svg" alt="">
+	</div>
+	<div class="inputContainer">
+		<input type="text" id="password" placeholder="Password" class="inputField"><img src="img/lock.svg" alt="">
+	</div>
+	<div class="twiceBtnContainer">
+		<button class="focusBtn">Sign up</button>
+	</div>
 `;
 }
