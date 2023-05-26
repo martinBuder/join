5b5f55153aae67a87d5cbba37606f516a1d6aba2
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-05-26 16:37:55 +0200

    Additions in add_task

--- js/add_task.js
+++ js/add_task.js
@@ -2,3 +2,29 @@ function toggleSelection() {
     let test = document.getElementById('testCategory');
     test.classList.toggle('d-none');
 }
+
+
+
+
+
+
+
+function clickReaction(item) {
+    // Fügen Sie hier den Code ein, der bei einem Klick auf einen Eintrag ausgeführt werden soll
+    console.log(item.textContent);
+}
+
+function toggleDropdownList() {
+    var dropdownList = document.getElementById("dropdownList");
+    var dropdownItems = dropdownList.getElementsByTagName("li");
+    
+    for (var i = 0; i < dropdownItems.length; i++) {
+        var item = dropdownItems[i];
+        
+        if (item.style.display === "none") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
