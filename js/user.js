9069bd7d30a11b471d8c0de0b45d6f0a30651c2d
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-06-01 12:30:43 +0200

    added mobil header

--- js/user.js
+++ js/user.js
@@ -1,4 +1,4 @@
-let token = IOGUK15K4QR1H4E6Z8358ZBEFF667PNX3VN95C2Z
+let token = 'IOGUK15K4QR1H4E6Z8358ZBEFF667PNX3VN95C2Z'
 
 let user = {
 	"token": token,
@@ -7,4 +7,17 @@ let user = {
 	"password": '',
 	"img": '',
 	"taska": [],
+}
+
+function createNewAccount() {
+	getUserInputFields();
+	console.log(username.value, email.value, password.value)
+}
+
+function getUserInputFields() {
+	let username = document.getElementById('name');
+	let email = document.getElementById('email');
+	let password = document.getElementById('password');
+
+	return {username, email, password};
 }
