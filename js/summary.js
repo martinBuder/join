9d884f3191826960579737b1b2f25a5a134de090
GinoEm <m-bell@gmx.de>
2023-06-01 14:08:18 +0200

    bildwechsel

---
+++ js/summary.js
@@ -0,0 +1,27 @@
+/**
+ * This function change the image by hover.
+ */
+document.getElementById('cha1').onmouseover = function changeImg1() {
+    document.getElementById('cha1Img').src = './img/Frame 592n.svg'
+}
+
+/**
+ * This function reverses the change by no-hover.
+ */
+document.getElementById('cha1').onmouseout = function changeImgBack1() {
+    document.getElementById('cha1Img').src = './img/Frame 592.svg'
+}
+
+/**
+ * This function change the image by hover.
+ */
+document.getElementById('cha2').onmouseover = function changeImg1() {
+    document.getElementById('cha2Img').src = './img/Frame 593n.svg'
+}
+
+/**
+ * This function reverses the change by no-hover.
+ */
+document.getElementById('cha2').onmouseout = function changeImgBack1() {
+    document.getElementById('cha2Img').src = './img/Frame 593.svg'
+}
