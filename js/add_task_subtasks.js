2441262437abb3ef40849ec3e76068864aaa37ae
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-06-02 16:54:12 +0200

    Subtask selection added

--- js/add_task_subtasks.js
+++ js/add_task_subtasks.js
@@ -1,3 +1,5 @@
+let selectedSubtasks = [];
+
 function selectSubtask() {
     toggleSubtaskVisibility();
     document.getElementById('newSubtaskInputField').focus();
@@ -11,10 +13,40 @@ function toggleSubtaskVisibility() {
     }
 }
 
+function toggleSubtaskCheck(subtask, imgId) {
+    let elem = document.getElementById(imgId);
+    if (elem.src.includes('unchecked')) {
+        elem.src = "./img/add-task/check-rectangle-checked.svg"
+        // selectedContacts.push(item);
+    } else {
+        elem.src = "./img/add-task/check-rectangle-unchecked.svg";
+        removeSubtask(subtask);
+    }
+    console.log(selectedSubtasks);
+}
+
 function cancelNewSubtaskInput() {
     toggleSubtaskVisibility();
+    document.getElementById('newSubtaskInputField').value = '';
 }
 
 function selectNewSubtaskInput() {
+    let elem = document.getElementById('newSubtaskInputField');
+    selectedSubtasks.push(elem.value);
+    let newCode = `
+        <li onclick="toggleSubtaskCheck('${elem.value}', 'check-${elem.value.replace(' ', '_')}')"><img src="./img/add-task/check-rectangle-checked.svg" alt="" id="check-${elem.value.replace(' ', '_')}">${elem.value}</li>
+    `;
+    document.getElementById('newSubtaskList').innerHTML += newCode;
+    console.log(selectedSubtasks);
+    cancelNewSubtaskInput();
+}
 
+function removeSubtask(subtask) {
+    selectedSubtasks = selectedSubtasks.filter((tmpItem) => tmpItem != subtask);
+    let id = `check-${subtask.replace(' ', '_')}`;
+    let img = document.getElementById(id);
+    let li = img.parentNode;
+    li.parentNode.removeChild(li);
+
+    console.log(selectedSubtasks);
 }
