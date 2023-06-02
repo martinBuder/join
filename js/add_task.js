b0ae31641069283db0617d43aa7ec97a03b64fb8
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-06-02 12:33:06 +0200

    Priority added in add task

--- js/add_task.js
+++ js/add_task.js
@@ -22,6 +22,7 @@ let categories = [
  */
 let selectedColour = '';
 let selectedCategory = '';
+let selectedPriority = '';
 
 
 /**
@@ -164,4 +165,37 @@ function selectNewCatInput() {
     addCategories();
     cancelNewCatInput();
     toggleSelection('category');
+}
+
+
+function selectPriority(prio) {
+    selectedPriority == prio ? selectedPriority = '' : selectedPriority = prio;
+    // Toggle selection
+    document.getElementById(`btnPrio${prio}`).classList.toggle('btnSelected');
+    document.getElementById(`btnPrio${prio}`).classList.toggle('btnSelected' + prio);
+    document.getElementById(`img${prio}`).classList.toggle('btnSelectedImage');
+    // Deactivate other buttons (if selected before)
+    let btnArray = ['Urgent', 'Medium', 'Low'];
+    for (let i = 0; i < btnArray.length; i++) {
+        if (btnArray[i] != prio) {
+            document.getElementById(`btnPrio${btnArray[i]}`).classList.remove('btnSelected');
+            document.getElementById(`btnPrio${btnArray[i]}`).classList.remove('btnSelected' + btnArray[i]);
+            document.getElementById(`img${btnArray[i]}`).classList.remove('btnSelectedImage');
+        }
+    }
+    console.log('Prio: ' + selectedPriority);
+}
+
+
+async function createTask() {
+    // Save task data
+    // TODO - SPEICHERN DER DATEN INKL. STATUS
+    // Start animation of confirmation button
+    let btn = document.getElementById('btnTaskAdded');
+    btn.classList.add('taskButtonsFlex');
+    btn.classList.remove('d-none');
+    btn.classList.add('w3-animate-bottom');
+    // Wait a second, than open board
+    await new Promise(wait => setTimeout(wait, 1000));
+    window.open('./board.html', '_self');
 }
