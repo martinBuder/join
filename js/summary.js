20675c445a04c2e9c005202cbdd8afef5cb11a9d
GinoEm <m-bell@gmx.de>
2023-06-05 14:39:42 +0200

    kommentiert

--- js/summary.js
+++ js/summary.js
@@ -30,10 +30,19 @@ document.getElementById('cha2').onmouseout = function changeImgBack1() {
 
 let userAsText = localStorage.getItem('user');
 let userName = '';
+
+/**
+ * If "userAsText" has a content, it will be converted to object and stored in "userName".
+ */
 if (userAsText) {
     userName = JSON.parse(userAsText);
 }
+
 document.getElementById('person').innerHTML = '';
+
+/**
+ * If "userName" is null, "Guest" is output, otherwise the content of "userName" is output.
+ */
 if (userName == 0) {
     document.getElementById('person').innerHTML += `Guest`;
 } else {
