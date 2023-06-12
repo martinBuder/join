0af2f7a86e4eccedfd7ecf44a2b1747e0acace28
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-06-12 11:20:11 +0200

    added new contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,108 +1,10 @@
-let contactList = [
-	{
-			name: 'Max Mustermann',
-			email: 'mustermann@web.de',
-			phonenumber: '0151 1234567',
-			initials: 'MM',
-			img: './img/person.svg',
-			color: 'bgColorFF0000',
-	},
-	{
-			name: 'Alfred Meier',
-			email: 'alfmeier@gmx.de',
-			phonenumber: '0151 1458734',
-			initials: 'AM',
-			img: './img/person.svg',
-			color: 'bgColor019623',
-	},
-	{
-			name: 'John Doe',
-			email: 'john.doe@example.com',
-			phonenumber: '0151 12345678',
-			initials: 'JD',
-			img: './img/john.svg',
-			color: 'bgColor0190E0',
-	},
-	{
-			name: 'Jane Smith',
-			email: 'jane.smith@example.com',
-			phonenumber: '0172 23456789',
-			initials: 'JS',
-			img: './img/jane.svg',
-			color: 'bgColorEE00D6',
-	},
-	{
-			name: 'Michael Johnson',
-			email: 'michael.johnson@example.com',
-			phonenumber: '0160 34567890',
-			initials: 'MJ',
-			img: './img/michael.svg',
-			color: 'bgColor02CF2F',
-	},
-	{
-			name: 'Emily Davis',
-			email: 'emily.davis@example.com',
-			phonenumber: '0152 45678901',
-			initials: 'ED',
-			img: './img/emily.svg',
-			color: 'bgColorFF7A00',
-	},
-	{
-			name: 'David Wilson',
-			email: 'david.wilson@example.com',
-			phonenumber: '0176 56789012',
-			initials: 'DW',
-			img: './img/david.svg',
-			color: 'bgColor9327FF',
-	},
-	{
-			name: 'Sophia Martinez',
-			email: 'sophia.martinez@example.com',
-			phonenumber: '0155 67890123',
-			initials: 'SM',
-			img: './img/sophia.svg',
-			color: 'bgColor69C4EB',
-	},
-	{
-			name: 'Daniel Anderson',
-			email: 'daniel.anderson@example.com',
-			phonenumber: '0162 78901234',
-			initials: 'DA',
-			img: './img/daniel.svg',
-			color: 'bgColorFC71FF',
-	},
-	{
-			name: 'Olivia Taylor',
-			email: 'olivia.taylor@example.com',
-			phonenumber: '0157 89012345',
-			initials: 'OT',
-			img: './img/olivia.svg',
-			color: 'bgColorAF1616',
-	},
-	{
-			name: 'Ethan Brown',
-			email: 'ethan.brown@example.com',
-			phonenumber: '0171 01234567',
-			initials: 'EB',
-			img: './img/ethan.svg',
-			color: 'bgColor462F8A',
-	},
-	{
-			name: 'Ava Clark',
-			email: 'ava.clark@example.com',
-			phonenumber: '0159 12345678',
-			initials: 'AC',
-			img: './img/ava.svg',
-			color: 'bgColor019623',
-	},
-];
+let contactList = [];
 
 let contactJson = {
 	name: 'person',
 	email: '',
 	phonenumber: '',
 	initials:'',
-	img: './img/person.svg',
 	color: '',
 }
 
@@ -113,6 +15,12 @@ function sortContacts() {
 		return 0;
 	})
 }
+
+async function initContact() {
+	  await init();
+			fillContactList();
+	};
+
 
 function fillContactList() {
 	let firstLetter
@@ -210,7 +118,7 @@ function closeAddContact() {
 	* @returns to fillContactList
 	*/
 function getFirstLetter(contact) {
-	return contact['name'].substr(0, 1)
+	return contact['name'].substring(0, 1)
 }
 
 /** check if the firstLetters are the same if not he fill the h2 with first Letter
@@ -228,6 +136,51 @@ function fillFirstLetterHeader(firstLetterHeader, firstLetter) {
 	}
 return firstLetterHeader
 }
+
+
+async function saveNewContact() {
+	getNewContactData();
+	getNewContactInitials();
+	addColorNewContact();
+	pushContactToContacts();
+	sortContacts()
+	saveContactList();
+	await setContactListToRemoteStorage('contactList', contactList);
+	getContactList();
+	fillContactList();
+	closeAddContact();
+}
+
+function getNewContactData() {
+	let contactName = document.getElementById('contactName').value;
+	let contactEmail = document.getElementById('contactEmail').value;
+	let contactPhone = document.getElementById('contactPhone').value;
+	contactJson['name'] = contactName;
+	contactJson['email'] = contactEmail;
+	contactJson['phonenumber'] = contactPhone;
+}
+
+function getNewContactInitials() {
+	let nameArray = contactJson['name'].split(' ');
+	let initial = '';
+	nameArray.length == 1 ? initial = nameArray[0].substring(0, 2) : initial = nameArray[0].substring(0, 1) + nameArray[1].substring(0, 1);
+	contactJson['initials'] = initial.toUpperCase();
+}
+
+function addColorNewContact() {
+	contactJson['color'] = getRandomColor();
+}
+
+function getRandomColor() {
+  const randomIndex = Math.floor(Math.random() * bgColorArray.length);
+  return bgColorArray[randomIndex];
+}
+
+function pushContactToContacts() {
+	contactList.push(contactJson);
+}
+
+
 
 
 
