8111e6081ee1e1ac3674d415b76df4e42c4f62fa
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-06-15 08:58:07 +0200

    Updates in view of a task

--- js/board.js
+++ js/board.js
@@ -191,9 +191,94 @@ async function moveTo(cardColumn) {
 // Viewing a task
 function openTaskForView(taskId) {
     console.log("ID des Tasks: " + taskId);
+
+    let newCode = getViewRender(taskId);
+
     document.getElementById('viewTask').classList.toggle('d-none');
+    document.getElementById('viewTaskContainer').innerHTML = newCode;
+
 }
 
 function closeTaskForView() {
     document.getElementById('viewTask').classList.toggle('d-none');
+}
+
+
+function getViewRender(taskId) {
+    let curTaskArray = tasksArray.filter(t => t['id'] == taskId);
+    let curTask = curTaskArray[0];
+    console.log(curTask);
+
+    let newCode = `
+    <div class="viewTaskHeadline">
+    <div id="viewTaskCategory" class="cardCategory bgCatColor${curTask['categorycolor']}">${curTask['category']}</div>
+            <img class="closeTaskView" onclick="closeTaskForView()" src="./img/icon-cancel.svg" alt="">
+        </div>
+        <div id="viewTaskTitle">
+            ${curTask['title']}
+        </div>
+        <div id="viewTaskDescription" class="mb-25">
+            ${curTask['description']}
+        </div>
+        ${getViewRenderSubtasks(curTask)}
+        <div class="viewTaskText viewTaskOneLine mb-25">
+            <b>Due date: </b>
+            <span id="viewTaskDueDate">${curTask['duedate']}</span>
+        </div>
+        <div id="viewTaskPriority" class="viewTaskText viewTaskOneLine mb-25">
+            <b>Priority:</b>
+            <button class="viewTaskPrio viewTaskPrio${curTask['prio']}">${curTask['prio']}<img src="./img/add-task/prio-${curTask['prio'].toLowerCase()}.svg"></button>
+        </div>
+        <div class="viewTaskText mb-25">
+            <b>Assigned To:</b>
+        </div>
+        <div id="viewTaskEditorList" class="viewTaskEditorList">
+            ${getViewRenderEditors(curTask)}
+        </div>
+    `;
+
+    return newCode;
+}
+
+function getViewRenderSubtasks(curTask) {
+    let subtaskCode = ``;
+    if (curTask['subtasks'].length > 0) {
+        subtaskCode += `
+        <div id="viewTaskSubtasks" class="viewTaskText mb-25">
+            <b>Subtasks:</b>
+            <ul class="newSubtaskList mt-10">
+        `;
+        for (let i = 0; i < curTask['subtasks'].length; i++) {
+            const elem = curTask['subtasks'][i];
+            subtaskCode += `
+                <li>
+                    <img src="./img/add-task/check-rectangle-${(elem['status'] == 'done' ? 'checked' : 'unchecked')}.svg" alt="">${elem['title']}
+                </li>
+            `;
+        }
+        subtaskCode += `
+            </ul>
+        </div>
+        `;
+    }
+    return subtaskCode;
+}
+
+function getViewRenderEditors(curTask) {
+    let editorsCode = ``;
+    for (let i = 0; i < curTask['assignedto'].length; i++) {
+        const elem = curTask['assignedto'][i];
+        console.log(elem);
+        let nameJson = contactList.filter(c => c['email'] == elem);
+        let name = nameJson.length > 0 ? nameJson[0]['initials'] : elem;
+        let color = nameJson.length > 0 ? nameJson[0]['color'] : 'bgColorGrey';
+        let nameInitials = getCardInitials(name);
+        editorsCode += `
+        <div class="viewTaskEditor viewTaskOneLine">
+            <div class="viewTaskEditorBadge ${color}">${nameInitials}</div>
+            <div class="viewTaskText">${elem}</div>
+        </div>
+        `;
+    }
+    return editorsCode;
 }
