16f3ed055bb10c18165d456613509d743906b6d4
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-06-16 16:22:52 +0200

    Task edit mode - rendering

--- js/board_edit_task.js
+++ js/board_edit_task.js
@@ -1,12 +1,12 @@
 // Classes for editing a task (edit mode)
-let currentEditedTask = '';
+let currentEditedTask = '';  // contains the id of the task that is currently edited
 
 function editTask(curId) {
     console.log('editTask gestartet - ID: ' + curId);
     currentEditedTask = curId;
     let curTask = tasksArray.filter(t => t['id'] == currentEditedTask);
 
-    closeTaskForView();
+    closeTaskForView();  // closes the viewing mode of the current task - otherwise it will still be open in the background
 
     // füllen der benötigten Felder, etc. mit den vorhandenen Daten
     document.getElementById('taskTitle').value = curTask[0]['title'];
@@ -21,23 +21,45 @@ function editTask(curId) {
 
     document.getElementById('taskDueDate').value = curTask[0]['duedate'];
 
+
+    boardSetEditors(curTask);
+    boardSetSubtasks(curTask);
+    boardChangeButtonVisibility();
+
+    addTask();  // opens the editing mode for a task - generally used when creating a new task, but here some data was added before
+}
+
+function saveTask() {
+    // Task mit der ID currentEditedTask speichern
+    let curTask = tasksArray.filter(t => t['id'] == currentEditedTask);
+
+    // Close Edit Form
+    closeAdd();
+
+    // Nach Save: Buttons zurückändern
+    document.getElementById('btnTaskClear').classList.remove('d-none');
+    document.getElementById('btnTaskCreate').classList.remove('d-none');
+    document.getElementById('btnTaskSave').classList.add('d-none');
+}
+
+// Help functions
+
+function boardSetEditors(curTask) {
     for (let i = 0; i < curTask[0]['assignedto'].length; i++) {
         const assignedto = curTask[0]['assignedto'][i];
-        // let pos = contactList.findIndex(co => co['email'] == assignedto);
         let listEntries = document.querySelectorAll('ul#newAssList li');
-        // console.log(listEntries);
         for (let j = 0; j < listEntries.length; j++) {
             const le = listEntries[j];
-            // console.log(le.getAttribute('onclick'));
             if (le.getAttribute('onclick').includes(assignedto)) {
                 le.click();
             }
         }
     }
+}
 
+function boardSetSubtasks(curTask) {
     for (let i = 0; i < curTask[0]['subtasks'].length; i++) {
-        const curSubtask = curTask[0]['subtasks'][i];
-        console.log(curSubtask);
+        let curSubtask = curTask[0]['subtasks'][i];
         selectedSubtasks.push(curSubtask['title']);
         selectedSubtasksStatus.push(curSubtask['status']);
 
@@ -52,29 +74,10 @@ function editTask(curId) {
         `;
         document.getElementById('newSubtaskList').innerHTML += newCode;
     }
-
-
-    // Buttons ändern
-    document.getElementById('btnTaskClear').classList.add('d-none');
-    document.getElementById('btnTaskCreate').classList.add('d-none');
-    document.getElementById('btnTaskSave').classList.remove('d-none');
-
-    addTask();
-
-
-
-    
 }
 
-function saveTask() {
-    // Task mit der ID currentEditedTask speichern
-    let curTask = tasksArray.filter(t => t['id'] == currentEditedTask);
-
-    // Close Edit Form
-    closeAdd();
-
-    // Nach Save: Buttons zurückändern
-    document.getElementById('btnTaskClear').classList.remove('d-none');
-    document.getElementById('btnTaskCreate').classList.remove('d-none');
-    document.getElementById('btnTaskSave').classList.add('d-none');
+function boardChangeButtonVisibility() {
+    document.getElementById('btnTaskClear').classList.add('d-none');   // hide clear button
+    document.getElementById('btnTaskCreate').classList.add('d-none');  // hide create button - click here would create a complete new task with the data
+    document.getElementById('btnTaskSave').classList.remove('d-none'); // show save button - click here will overwrite already saved data
 }
