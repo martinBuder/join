cb87d8eb1d0ebd59a9c72eabdb28ab5525cef195
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-06-16 08:49:38 +0200

    slide wondow

--- js/taskSection.js
+++ js/taskSection.js
@@ -2,17 +2,11 @@
  * This function opens the input mask.
  */
 function addTask() {
-  let addTaskWindow = document.getElementById("addTaskSection");
-  addTaskWindow.style.animationName = "slide";
-  addTaskWindow.style.animationDuration = "1.5s";
-  addTaskWindow.style.animationFillMode = "forwards";
+  document.getElementById('addTask').classList.remove('addTask-none');
 }
 /**
- * This function closes the input mask.
- */
+* This function closes the input mask.
+*/
 function closeAdd() {
-  let addTaskWindow = document.getElementById("addTaskSection");
-  addTaskWindow.style.animationName = "slideOut";
-  addTaskWindow.style.animationDuration = "1.5s";
-  addTaskWindow.style.animationFillMode = "backwards";
+  document.getElementById('addTask').classList.add('addTask-none');
 }
