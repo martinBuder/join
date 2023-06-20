45d06cbc0a89d5c874ab96a8c332f5d8667c7167
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-06-20 11:45:33 +0200

    css changes after feedback

--- js/slideSection.js
+++ js/slideSection.js
@@ -6,10 +6,10 @@ function addTask() {
   document.getElementById('editOutScroll').style.overflow = "hidden";
 }
 /**
- * This function closes the input mask.
+ * This function closes the input mask and clears all global variables that were used.
  */
 function closeAdd() {
   document.getElementById('addTask').classList.add('addTask-none');
   document.getElementById('editOutScroll').style.overflow = "auto";
-  clearTask(); // deletes all entries - needed in editing an existing task
+  clearTask(); // deletes all entries, that is all global variables that are needed in editing an existing task
 }
