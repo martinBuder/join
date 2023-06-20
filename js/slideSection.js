32c184d30fe854550b49184bf4a268a510f34219
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-06-20 10:20:10 +0200

    added no outer scroll

--- js/slideSection.js
+++ js/slideSection.js
@@ -3,11 +3,13 @@
  */
 function addTask() {
   document.getElementById('addTask').classList.remove('addTask-none');
+  document.getElementById('editOutScroll').style.overflow = "hidden";
 }
 /**
  * This function closes the input mask.
  */
 function closeAdd() {
   document.getElementById('addTask').classList.add('addTask-none');
+  document.getElementById('editOutScroll').style.overflow = "auto";
   clearTask(); // deletes all entries - needed in editing an existing task
 }
